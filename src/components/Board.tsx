90cf9b1333a27162fb2c7f8414022f0b74cc799a
ddubinin <dubinin.dmitry@mail.ru>
2023-03-14 14:26:38 +0300

    feat: create board

---
+++ src/components/Board.tsx
@@ -0,0 +1,29 @@
+import Knight from "./Knight";
+import Square from "./Square";
+
+function renderSquare(i: any, [knightX, knightY]: any) {
+  const x = i % 8;
+  const y = Math.floor(i / 8);
+  const isKnigthHere = x === knightX && y === knightY;
+  const black = (x + y) % 2 === 1;
+  const place = isKnigthHere ? <Knight /> : null;
+
+  return (
+    <div key={i} className="square-wrap">
+      <Square black={black}>{place}</Square>
+    </div>
+  );
+}
+
+export default function Board({ position }: any) {
+  const squares = []
+
+  for(let i = 0; i < 64; i++) {
+    squares.push(renderSquare(i, position))
+  }
+  return (
+    <div className="board">
+      {squares}
+    </div>
+  );
+}
