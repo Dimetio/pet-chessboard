905104d540c20a0c6e907e2031ebe98f345d46a8
ddubinin <dubinin.dmitry@mail.ru>
2023-03-14 20:56:33 +0300

    feat: added previe img

--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -1,16 +1,20 @@
 import { useDrop } from "react-dnd";
 import { ItemTypes } from "../constants";
-import { moveKnight } from "../game";
+import { canMoveKnight, moveKnight } from "../game";
 import Square from "./Square";
 
 export default function BoardSquare({ x, y, children }: any) {
   const black = (x + y) % 2 === 1;
 
-  const [{ isOver }, drop] = useDrop(
+  const [{ isOver, canDrop }, drop] = useDrop(
     () => ({
       accept: ItemTypes.KNIGHT,
+      canDrop: () => canMoveKnight(x, y),
       drop: () => moveKnight(x, y),
-      collect: (monitor) => ({ isOver: !!monitor.isOver() }),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+        canDrop: !!monitor.canDrop(),
+      }),
     }),
     [x, y]
   );
@@ -18,7 +22,9 @@ export default function BoardSquare({ x, y, children }: any) {
   return (
     <div className="square-wrap" ref={drop}>
       <Square black={black}>{children}</Square>
-      {isOver && <div className="square-yellow"></div>}
+      {isOver && !canDrop && <div className="overlay square-red"></div>}
+      {!isOver && canDrop && <div className="overlay square-yellow"></div>}
+      {isOver && canDrop && <div className="overlay square-green"></div>}
     </div>
   );
 }
