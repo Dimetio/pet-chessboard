a052ab20248705b1e549897a985626ee60fee487
ddubinin <dubinin.dmitry@mail.ru>
2023-03-14 14:26:57 +0300

    feat: show chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
+      <Board position={[0,0]}/>
     </div>
   );
 }
